4a7879ac1d4f4312f660f52aad36619f6214fe62
Maxime <maximehaudegond@gmail.com>
2016-10-19 10:29:18 -0400

    TP3
    Creature Magique

--- TP3/CreatureMagique.cpp
+++ TP3/CreatureMagique.cpp
@@ -1,13 +1,54 @@
 #include "CreatureMagique.h"
 
+//Constructeur par parametres
 CreatureMagique::CreatureMagique(const string& nom, unsigned int attaque,
 		unsigned int defense, unsigned int pointDeVie, unsigned int energie, unsigned int bonus):Creature(nom,attaque,defense,pointDeVie, energie),bonus_(bonus)
 {}
-
+//Destructeur
 CreatureMagique::~CreatureMagique()
 {}
-
+//constructeur par copie
 CreatureMagique::CreatureMagique(const CreatureMagique& creatureMagique)
 {
+    Creature(creatureMagique);
+    etat_=new EtatCreature(*creatureMagique.etat_);
+}
+//Accesseurs
+int CreatureMagique::getBonus() const
+{
+    return bonus_;
+}
+//Modificateurs
+void CreatureMagique::setBonus(int bonus)
+{
+    bonus_=bonus;
+}
+//operators
+ostream& operator<<(ostream& os, const CreatureMagique& creatureMagique)
+{
+    Creature::operator<<(os,creatureMagique);
+    os<<"Et "<<creatureMagique.nom_<<" a aussi un bonus de "<<creatureMagique.bonus_;
+    return os;
+}
 
+CreatureMagique& CreatureMagique::operator=(const CreatureMagique& creatureMagique)
+{
+    if(this != &creatureMagique){
+        Creature::operator=(creatureMagique);
+        delete etat_;
+        etat_=nullptr;
+        etat_=new EtatCreature(*creatureMagique.etat_);
+    }
+    return *this;
+}
+//Methode attaquer surchargée
+void CreatureMagique::attaquer(const Pouvoir& pouvoir, Creature& creature)
+{
+    Creature::attaquer(pouvoir, creature);
+    if(pointDeVie_+bonus_>=pointDeVieTotal_){
+        pointDeVie_=pointDeVieTotal_;
+    }
+    else{
+        pointDeVie_+=bonus_;
+    }
 }
--- TP3/CreatureMagique.h
+++ TP3/CreatureMagique.h
@@ -11,16 +11,27 @@ Description: les creatures magiques sont des creatures avec un bonus
 
 #include "Creature.h"
 
+using namespace std;
 
 class CreatureMagique : public Creature
 {
     public:
+        //Constructeur par parametres
         CreatureMagique(const string& nom, unsigned int attaque,
 		unsigned int defense, unsigned int pointDeVie, unsigned int energie, unsigned int bonus);
+		//Desstructeur
         ~CreatureMagique();
+        //Constructeur par copie
         CreatureMagique(const CreatureMagique& creatureMagique);
-        int getbonus_() { return bonus_; }
-        void setbonus_(int val) { bonus_ = val; }
+        //Accesseurs/Modificateurs
+        int getBonus() const;
+        void setBonus(int bonus);
+        //Méthode surchargée
+        void attaquer(const Pouvoir& pouvoir, Creature& creature);
+        //operator
+        CreatureMagique& operator=(const CreatureMagique& creatureMagique);
+        friend ostream& operator<<(ostream& os, const CreatureMagique& creatureMagique);
+
     private:
         unsigned int bonus_;
 };
