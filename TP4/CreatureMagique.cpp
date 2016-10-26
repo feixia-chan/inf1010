492da614f43d7dd24dd7c857ec5378514999c58d
Maxime <maximehaudegond@gmail.com>
2016-10-26 10:41:34 -0400

    TP4
    créature Magique

---
+++ TP4/CreatureMagique.cpp
@@ -0,0 +1,97 @@
+#include "CreatureMagique.h"
+
+//Constructeur par défaut
+CreatureMagique::CreatureMagique(): Creature()
+{
+    bonus_=0;
+    attaqueMagique_=nullptr;
+}
+
+//constructeur par paramètres
+CreatureMagique::CreatureMagique(const string nom, unsigned int attaque, unsigned int defense,
+                                 unsigned int pointDeVie, unsigned int energie, unsigned int bonus):
+                                     Creature(nom,attaque,defense,pointDeVie,energie), bonus_(bonus)
+{
+    attaqueMagique_=nullptr;
+}
+CreatureMagique::~CreatureMagique()
+{
+    delete attaqueMagique_;
+    attaqueMagique_=nullptr;
+}
+
+//constructeur par copie
+CreatureMagique::CreatureMagique(const CreatureMagique& creatureMagique): Creature(creatureMagique)
+{
+    bonus_=creatureMagique.bonus_;
+    delete attaqueMagique_;
+    attaqueMagique_=nullptr;
+    attaqueMagique_=new AttaqueMagique(*typeid(creatureMagique.attaqueMagique_));
+}
+
+//Accesseurs
+AttaqueMagique* CreatureMagique::getAttaqueMagique() const
+{
+    return attaqueMagique_;
+}
+
+unsigned int CreatureMagique::getBonus() const
+{
+    return bonus_;
+}
+
+//Modificateurs
+void CreatureMagique::setBonus(unsigned int bonus)
+{
+    bonus_=bonus;
+}
+
+//Type de la créature
+string CreatureMagique::getTypeCreature() const
+{
+    return typeid(CreatureMagique).name;
+}
+
+//Attaquer
+void attaquer(const Pouvoir& pouvoir, Creature& creature)
+{
+    if(!attaqueMagique_->estFini())
+    {
+        attaqueMagique_->appliquerAttaque(creature);
+    }
+    if(pointDeVie_+bonus_<pointDeVieTotal_)
+    {
+       pointDeVie_+=bonus_;
+    }
+    else
+    {
+        pointDeVie_=pointDeVieTotal_;
+    }
+    Creature::attaquer(pouvoir, creature);
+
+}
+//operator
+CreatureMagique::operator=(const CreatureMagique& creatureMagique)
+{
+    if(this != &creatureMagique)
+    {
+       Creature::operator=(creatureMagique);
+       bonus_=creatureMagique.bonus_;
+       delete attaqueMagique_;
+       attaqueMagique_=nullptr;
+       attaqueMagique_=new AttaqueMagique(*typeid(creatureMagique.attaqueMagique_));
+    }
+    return *this;
+
+}
+
+//Apprentissage et oubli d'attaque magique
+void CreatureMagique::apprendreAttaqueMagique(AttaqueMagique* attaqueMagique)
+{
+    attaqueMagique_=new AttaqueMagique(*attaqueMagique);
+}
+
+void CreatureMagique::oublierAttaqueMagique()
+{
+    delete attaqueMagique_;
+}
---
+++ TP4/CreatureMagique.h
@@ -0,0 +1,47 @@
+/*
+Fichier: CreatureMagique.h
+Auteur(s): Alexandra LIN et Maxime HAUDEGOND
+Date de creation: 26 octobre 2016
+Description: Les creatures magiques sont des etres destines au combat et possèdent des attaques magiques
+*/
+
+#ifndef CREATUREMAGIQUE_H
+#define CREATUREMAGIQUE_H
+
+#include "Creature.h"
+
+using namespace std;
+
+class CreatureMagique : public Creature
+{
+    public:
+        CreatureMagique();
+        CreatureMagique(const string nom, unsigned int attaque, unsigned int defense,
+                         unsigned int pointDeVie, unsigned int energie, unsigned int bonus);
+        ~CreatureMagique();
+        CreatureMagique(const CreatureMagique& creatureMagique);
+
+        //Accesseurs et Modificateurs
+        AttaqueMagique* getAttaqueMagique() const;
+        unsigned int getBonus() const;
+        void setBonus(unsigned int bonus);
+
+        //Type de la créature
+        string getTypeCreature() const;
+
+        //Attaquer
+        void attaquer(const Pouvoir& pouvoir, Creature& creature);
+
+        //Appentissage et oubli d'attaque magique
+        void apprendreAttaqueMagique(AttaqueMagique* attaqueMagique);
+        void oublierAttaqueMagique();
+
+        //operator
+        operator=(const CreatureMagique& creatureMagique);
+
+    private:
+        AttaqueMagique* attaqueMagique_;
+        unsigned int bonus_;
+};
+
+#endif // CREATUREMAGIQUE_H
