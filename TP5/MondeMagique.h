73888ac759baedb58dd4608c06ec21ed4bbefce4
Pinkachu <alexandra.lin@hotmail.fr>
2016-11-09 09:24:32 -0500

    TP5
    MM : methodes ajouter

--- TP5/MondeMagique.h
+++ TP5/MondeMagique.h
@@ -21,47 +21,72 @@ using namespace std;
 template <typename T, typename S>
 class MondeMagique
 {
-    public:
-        MondeMagique() {}
-        virtual ~MondeMagique() {}
-        //accesseurs
-        list <T*> getListMaitre() const;
-        list <S*> getListCompagnon()const;
+public:
+    MondeMagique() {}
+    virtual ~MondeMagique() {}
+    //accesseurs
+    template<typename T>
+    list <T*> getListMaitre() const;
+    template<typename S>
+    list <S*> getListCompagnon()const;
 
-        void setListMaitre(list <T*> listeM);
-        void setListCompagnon(<list <S*> listeC);
+    template< typename T >
+    void setListMaitre(list <T*> listeM);
+    template< typename S >
+    void setListCompagnon(<list <S*> listeC);
+
+    //méthodes
+    template< typename T>
+    void ajouter (T* maitre); //ajoute un maitre dans la liste
+    template< typename S >
+    void ajouter S* compagnon);// ajoute une créature dans la liste
 
 
-    protected:
+protected:
     list <T*> listMaitre_;  //liste de maîtres
     list <S*> listCompagnon_;  //liste de compagnons
-    private:
+private:
 };
 
 template< typename T, typename S >
-MondeMagique<T,S>::MondeMagique(){}
+MondeMagique<T,S>::MondeMagique() {}
 template< typename T, typename S >
-MondeMagique<T,S>::~MondeMagique(){
+MondeMagique<T,S>::~MondeMagique()
+{
 
 }
 
 //accesseurs
- template< typename T, typename S >
-MondeMagique<T,S>::list <T*> getListMaitre() const{
+template< typename T>
+MondeMagique<T,S>::list <T*> getListMaitre() const
+{
     return listMaitre_;
- }
+}
 
-template< typename T, typename S >
+template< typename S >
 MondeMagique<T,S>::list <S*> getListCompagnon()const
 
-template< typename T, typename S >
-MondeMagique<T,S>::void setListMaitre(list <T*> listeM){
+template< typename T >
+MondeMagique<T,S>::void setListMaitre(list <T*> listeM)
+{
     listMaitre_ = listeM;
 }
-template< typename T, typename S >
-MondeMagique<T,S>::void setListCompagnon(<list <S*> listeC){
+template< typename S >
+MondeMagique<T,S>::void setListCompagnon(<list <S*> listeC)
+{
     listCompagon_=listeC;
 }
 
+
+template< typename T>
+MondeMagique<T,S>::void ajouter (T* maitre){
+    listMaitre_.push_back(maitre);
+
+}
+template< typename S >
+MondeMagique<T,S>::void ajouter S* compagnon)
+{
+    listCompagnon_.push_back(compagnon);
+}
 
 #endif // MONDEMAGIQUE_H
