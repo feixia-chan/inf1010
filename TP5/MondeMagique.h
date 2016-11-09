3336c6d72fb326e6b36458527922c0d5eef8777d
Pinkachu <alexandra.lin@hotmail.fr>
2016-11-09 09:06:59 -0500

    TP5
    monde magique avec accesseurs

--- TP5/MondeMagique.h
+++ TP5/MondeMagique.h
@@ -24,18 +24,43 @@ class MondeMagique
     public:
         MondeMagique() {}
         virtual ~MondeMagique() {}
+        //accesseurs
+        list <T*> getListMaitre() const;
+        list <S*> getListCompagnon()const;
+
+        void setListMaitre(list <T*> listeM);
+        void setListCompagnon(<list <S*> listeC);
 
 
     protected:
     list <T*> listMaitre_;  //liste de maîtres
-    list <S*> listeCompagnon_;  //liste de compagnons
+    list <S*> listCompagnon_;  //liste de compagnons
     private:
 };
 
 template< typename T, typename S >
 MondeMagique<T,S>::MondeMagique(){}
-~MondeMagique<T,S>::MondeMagique(){
+template< typename T, typename S >
+MondeMagique<T,S>::~MondeMagique(){
 
+}
+
+//accesseurs
+ template< typename T, typename S >
+MondeMagique<T,S>::list <T*> getListMaitre() const{
+    return listMaitre_;
+ }
+
+template< typename T, typename S >
+MondeMagique<T,S>::list <S*> getListCompagnon()const
+
+template< typename T, typename S >
+MondeMagique<T,S>::void setListMaitre(list <T*> listeM){
+    listMaitre_ = listeM;
+}
+template< typename T, typename S >
+MondeMagique<T,S>::void setListCompagnon(<list <S*> listeC){
+    listCompagon_=listeC;
 }
 
 
