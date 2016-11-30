5e7f65969ba796edf819dcc145f6408eb8a96df6
Lin Alexandra <alexandra.lin@hotmail.fr>
2016-11-30 10:23:34 -0500

    merging

---
+++ TP6/TP6/exceptionecheccapture.h
@@ -0,0 +1,17 @@
+#ifndef EXCEPTIONECHECCAPTURE_H
+#define EXCEPTIONECHECCAPTURE_H
+using namespace std;
+class ExceptionEchecCapture : public runtime_error
+{
+    //lancée lors de l’échec d’une attaque à cause du manque d’énergie de la créature
+public:
+ ExceptionEchecCapture(string what_arg = "");
+
+};
+
+ExceptionEchecCapture::ExceptionEchecCapture(string what_arg) : runtime_error(what_arg)
+{
+}
+
+
+#endif // EXCEPTIONECHECCAPTURE_H
