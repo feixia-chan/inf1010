cb0338caf9dbfa26f8204c5d92a2dd1a6cdd2757
Lin Alexandra <alexandra.lin@hotmail.fr>
2016-11-30 10:12:38 -0500

    TP6 en cours

---
+++ TP6/exceptionattaqueechouee.h
@@ -0,0 +1,18 @@
+#ifndef EXCEPTIONATTAQUEECHOUEE_H
+#define EXCEPTIONATTAQUEECHOUEE_H
+using namespace std;
+class ExceptionAttaqueEchouee : public runtime_error
+{
+    //lancée lors de l’échec d’une attaque à cause du manque d’énergie de la créature
+public:
+ ExceptionAttaqueEchouee(string what_arg = "");
+
+};
+
+ExceptionAttaqueEchouee::ExceptionAttaqueEchouee(string what_arg) : runtime_error(what_arg)
+{
+}
+
+
+
+#endif // EXCEPTIONATTAQUEECHOUEE_H
---
+++ TP6/exceptioncreaturemorte.h
@@ -0,0 +1,25 @@
+#ifndef EXCEPTIONCREATUREMORTE_H
+#define EXCEPTIONCREATUREMORTE_H
+
+using namespace std;
+class ExceptionCreatureMorte : public runtime_error
+{
+    // lancée lors de l’échec de l’attaque sur une créature car celle-ci n’a plus de point de vie.
+public:
+ ExceptionCreatureMorte(string what_arg = "");
+static int obtenirValeurCompteur(){return compteur_;}
+private:
+ static int compteur_;
+
+
+};
+
+int ExceptionCreatureMorte::compteur_=0; //init de l`attribut statique
+
+ExceptionCreatureMorte::ExceptionCreatureMorte(string what_arg) : runtime_error(what_arg)
+{
+    compteur_++;
+}
+
+
+#endif // EXCEPTIONCREATUREMORTE_H
